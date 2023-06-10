ea442774c30915410b1cedeca4cdc03498d8da3e
Saurabh Kumar <saurabh.kumar13@sap.com>
2023-06-10 21:49:28 +0530

    New Commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+    });
+});
